117c0a9b54ed3575a0a3ca9fa5f2a7af44a2a115
Yahaire <yahaire5@hotmail.com>
2015-11-23 18:35:49 -0600

    Added FoodClassification pList and FoodClassification cell class
    The pList contains the FoodItems to show in the FoodClassification
    table.
    
    The cell class will hold the outlets to show a better interface on the
    table.

---
+++ Kenkou/Kenkou/FoodClassificationTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  FoodClassificationTableViewCell.h
+//  
+//
+//  Created by Nelly on 23/11/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FoodClassificationTableViewCell : UITableViewCell
+
+@end
---
+++ Kenkou/Kenkou/FoodClassificationTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  FoodClassificationTableViewCell.m
+//  
+//
+//  Created by Nelly on 23/11/15.
+//
+//
+
+#import "FoodClassificationTableViewCell.h"
+
+@implementation FoodClassificationTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
